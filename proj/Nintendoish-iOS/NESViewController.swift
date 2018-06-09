56e3f6c474ec8bfd86c32f97a13d6b77af2f7087
Ingebretsen, Andrew (HBO) <Andrew.Ingebretsen@hbo.com>
2018-06-08 23:46:34 -0700

    More changes and clean up

--- proj/Nintendoish-iOS/NESViewController.swift
+++ proj/Nintendoish-iOS/NESViewController.swift
@@ -10,39 +10,34 @@ import UIKit
 import MetalKit
 
 // Our iOS specific view controller
-class GameViewController: UIViewController {
+class NESViewController: UIViewController {
 
     var renderer: Renderer!
-    var mtkView: MTKView!
+    @IBOutlet weak var metalView: MTKView!
     var nes: NESBridge!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let mtkView = view as? MTKView else {
-            print("View of Gameview controller is not an MTKView")
-            return
-        }
-
         // Select the device to render with.  We choose the default device
         guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
             print("Metal is not supported")
             return
         }
         
-        mtkView.device = defaultDevice
-        mtkView.backgroundColor = UIColor.black
+        metalView.device = defaultDevice
+        metalView.backgroundColor = UIColor.black
 
-        guard let newRenderer = Renderer(metalKitView: mtkView) else {
+        guard let newRenderer = Renderer(metalKitView: metalView) else {
             print("Renderer cannot be initialized")
             return
         }
 
         renderer = newRenderer
 
-        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+        renderer.mtkView(metalView, drawableSizeWillChange: metalView.drawableSize)
 
-        mtkView.delegate = renderer
+        metalView.delegate = renderer
         
         NESBridge.sharedNES().loadRom(Bundle.main.path(forResource: "Zelda", ofType: "nes"))
     }
