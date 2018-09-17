37b1f9e3c25450785844f255183a206ccab46ca8
Drew Ingebretsen <drewying@gmail.com>
2018-09-16 23:13:12 -0700

    Adding more tests

--- src/Desktop/main.cpp
+++ src/Desktop/main.cpp
@@ -242,7 +242,7 @@ int main(int argc, char** argv) {
     //* nes->loadProgram("roms/test/cpu/branch_timing_tests/1.Branch_Basics.nes");
     //* nes->loadProgram("roms/test/cpu/branch_timing_tests/2.Backward_Branch.nes");
     //* nes->loadProgram("roms/test/cpu/branch_timing_tests/3.Forward_Branch.nes");
-    //nes->loadProgram("roms/test/cpu/cpu_interrupts_v2/cpu_interrupts.nes");
+    nes->loadProgram("roms/test/cpu/cpu_interrupts_v2/cpu_interrupts.nes");
     //* nes->loadProgram("roms/test/cpu/cpu_reset/ram_after_reset.nes");
     //* nes->loadProgram("roms/test/cpu/cpu_reset/registers.nes");
     //* nes->loadProgram("roms/test/cpu/cpu_timing_test6/cpu_timing_test.nes");
@@ -280,7 +280,7 @@ int main(int argc, char** argv) {
     //* nes->loadProgram("roms/test/apu/apu_reset/irq_flag_cleared.nes");
     //* nes->loadProgram("roms/test/apu/apu_reset/len_ctrs_enabled.nes");
     //nes->loadProgram("roms/test/apu/apu_reset/works_immediately.nes");
-    nes->loadProgram("roms/test/apu/apu_test/apu_test.nes");
+    //nes->loadProgram("roms/test/apu/apu_test/apu_test.nes");
     //* nes->loadProgram("roms/test/apu/blargg_apu_2005.07.30/01.len_ctr.nes");
     //* nes->loadProgram("roms/test/apu/blargg_apu_2005.07.30/02.len_table.nes");
     //* nes->loadProgram("roms/test/apu/blargg_apu_2005.07.30/03.irq_flag.nes");
@@ -306,8 +306,16 @@ int main(int argc, char** argv) {
     //nes->loadProgram("roms/test/apu/test_apu_timers/square_pitch.nes");
     //nes->loadProgram("roms/test/apu/test_apu_timers/triangle_pitch.nes");
     //nes->loadProgram("roms/test/apu/test_tri_lin_ctr/lin_ctr.nes");
+    //nes->loadProgram("roms/test/apu/test_tri_lin_ctr/lin_ctr.nes");
 
-    //nes->loadProgram("roms/test/cpu/cpu_interrupts_v2/rom_singles/5-branch_delays_irq.nes");
+    //Mapper
+
+    //nes->loadProgram("roms/test/mapper/mmc3_test_2/rom_singles/1-clocking.nes");
+    //nes->loadProgram("roms/test/mapper/mmc3_test_2/rom_singles/2-details.nes");
+    //nes->loadProgram("roms/test/mapper/mmc3_test_2/rom_singles/3-A12_clocking.nes");
+    //nes->loadProgram("roms/test/mapper/mmc3_test_2/rom_singles/4-scanline_timing.nes");
+    //nes->loadProgram("roms/test/mapper/mmc3_test_2/rom_singles/5-MMC3.nes");
+    //nes->loadProgram("roms/test/mapper/mmc3_test_2/rom_singles/6-MMC3_alt.nes");
 
     //Games
     //nes->loadProgram("../roms/PunchOut.nes");
